b626d334c5c2a97e3e8ab61cb0cb7e13bc37ea6d
Locox-dev <316854@vassaux.net>
2023-08-27 13:53:23 +0200

    Modified installer.bat and cleaned the json files

--- installer.bat
+++ installer.bat
@@ -1,10 +1,19 @@
 @echo off
 
 REM Creating virtual environment for python
+echo Creating virtual environment for python
 python -m venv venv
 
 REM Activating the environment
 call venv\Scripts\activate.bat
 
 REM Installing packages
+echo Installing python packages from requirements.txt
 pip install -r requirements.txt
+
+REM Installing node packages
+echo Installing node packages from package.json
+call node install
+
+echo Installation DONE!
+echo You can now run start.bat to use the FlowCord tool!
